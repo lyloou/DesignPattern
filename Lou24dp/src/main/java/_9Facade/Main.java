62ea0d3b83f5caf58f49e129eb6793e189ca4c7e
Lou <lyloou6@gmail.com>
2017-10-11 17:48:18 +0800

    add _9Facade

---
+++ Lou24dp/src/main/java/_9Facade/Main.java
@@ -0,0 +1,10 @@
+package _9Facade;
+
+public class Main {
+    public static void main(String[] args) {
+        Sys sys = new Sys();
+        sys.combineM1();
+        sys.combineM2();
+        sys.combineM3();
+    }
+}
---
+++ Lou24dp/src/main/java/_9Facade/SubSys2.java
@@ -0,0 +1,15 @@
+package _9Facade;
+
+public class SubSys2 {
+    public void m1() {
+        System.out.println("Call : SubSys2 --- m1");
+    }
+
+    public void m2() {
+        System.out.println("Call : SubSys2 --- m2");
+    }
+
+    public void m3() {
+        System.out.println("Call : SubSys2 --- m3");
+    }
+}
