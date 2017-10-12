2439dc4c9b72d0a21876d43c4d631717d6d8a5b5
Lou <lyloou6@gmail.com>
2017-10-12 15:32:54 +0800

    add _CTemplate

---
+++ Lou24dp/src/main/java/_CTemplate/Main.java
@@ -0,0 +1,8 @@
+package _CTemplate;
+
+public class Main {
+    public static void main(String[] args) {
+        ConcreteFlow concreteFlow = new ConcreteFlow();
+        concreteFlow.go();
+    }
+}
